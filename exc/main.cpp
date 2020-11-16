2daf440a69678ab2db67fc40ec9a9b7d7bd7d66a
xtab-lang <603667+xtab-lang@users.noreply.github.com>
2020-11-16 16:46:08 +0300

    argc argv

--- exc/main.cpp
+++ exc/main.cpp
@@ -2,9 +2,10 @@
 
 import console;
 
-int main(int, char **) {
-
+int main(int argc, char **argv) {
 	console::writeln(S("Hello modules!"));
-
+	for (auto i = 0; i < argc; ++i) {
+		console::writeln(argv[i], strlen(argv[i]));
+	}
 	return 0;
 }
